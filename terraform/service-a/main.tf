f13bb6975fb717282fc6bd46733ff58ce85a689a
Chris Gradwohl <175059153+cgradwohl-playq@users.noreply.github.com>
2024-11-25 08:58:29 -0800

    feat:

--- terraform/service-a/main.tf
+++ terraform/service-a/main.tf
@@ -1,8 +1,14 @@
+resource "random_string" "suffix" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.2.2"
 
-  bucket = var.inputs.bucket
+  bucket = "${var.inputs.bucket}-${random_string.suffix.result}"
   acl    = var.inputs.acl
 
   control_object_ownership = var.inputs.control_object_ownership
