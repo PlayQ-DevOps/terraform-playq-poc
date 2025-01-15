7fc7ae294475e256e7c2c8cec60317307ea3f682
Chris Gradwohl <175059153+cgradwohl-playq@users.noreply.github.com>
2025-01-15 13:29:55 -0800

    Update variables.tf

--- terraform/service-a/variables.tf
+++ terraform/service-a/variables.tf
@@ -5,7 +5,7 @@ variable "bucket" {
 
 variable "acl" {
   type        = string
-  description = "Specify private or public acl."
+  description = "Specify private or public acl. THIS IS A TEST CHANGE."
 }
 
 variable "control_object_ownership" {
