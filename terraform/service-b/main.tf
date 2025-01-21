d54fcaad1bba159bb112ac51260544f65d3dac5b
Chris Gradwohl <175059153+cgradwohl-playq@users.noreply.github.com>
2025-01-21 05:28:27 -0800

    Update main.tf

--- terraform/service-b/main.tf
+++ terraform/service-b/main.tf
@@ -1,20 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+  required_version = ">= 1.8"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# TEST COMMIT
 resource "random_string" "suffix" {
   length  = 5
   upper   = false
-  special = true
+  special = false
 }
 
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.2.2"
 
-  bucket = "${var.inputs.bucket}-${random_string.suffix.result}"
-  acl    = var.inputs.acl
+  bucket = "${var.bucket}-${random_string.suffix.result}"
+  acl    = var.acl
 
-  control_object_ownership = var.inputs.control_object_ownership
-  object_ownership         = var.inputs.object_ownership
+  control_object_ownership = var.control_object_ownership
+  object_ownership         = var.object_ownership
 
   versioning = {
-    enabled = var.inputs.versioning.enabled
+    enabled = var.versioning.enabled
   }
 }
