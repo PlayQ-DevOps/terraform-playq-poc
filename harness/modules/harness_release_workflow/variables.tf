1755b8bb32c2d2e95eac8e0dcd7fa14f3e096b52
Chris Gradwohl <175059153+cgradwohl-playq@users.noreply.github.com>
2025-01-29 12:43:50 -0800

    feat

--- harness/modules/harness_release_workflow/variables.tf
+++ harness/modules/harness_release_workflow/variables.tf
@@ -1,10 +1,35 @@
+variable "changed_files" {
+  type        = string
+  description = "The comma separated list of files to trigger on."
+}
+
+variable "git_ref" {
+  type        = string
+  description = "The branch or tag to reference from the git repository."
+}
+
+variable "module_file_path" {
+  type        = string
+  description = "The relative file path from root to the module source code."
+}
+
 variable "name" {
   type        = string
   description = "The name of the workflow."
 }
 
-variable "changed_files_regex" {
+variable "org_id" {
   type        = string
-  description = "The regex expression of the file paths to watch."
+  description = "The harness organization identifier."
+}
+
+variable "project_id" {
+  type        = string
+  description = "The harness project identifier."
+}
+
+variable "variables_file_path" {
+  type        = string
+  description = "The relative file path from root to the tfvars file."
 }
 
